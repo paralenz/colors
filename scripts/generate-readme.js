b358214690cac1613bd0d0c1908a9ce7ea128e9f
Carsten Daurehøj <arelstone@gmail.com>
2022-02-24 09:35:57 +0100

    🔨 Generate scss colors on postinstall

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -1,5 +1,5 @@
 const { colors } = require( '../dist/index')
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 const { join } = require('path')
 
 const generateTableRow = (name, hex) => {
@@ -12,4 +12,6 @@ ${Object.entries(colors).map(([name, hex]) => generateTableRow(name, hex)).join(
 
 const content = readFileSync(join(__dirname, '..', '.github', 'templates', 'readme.tmpl'), { encoding: 'utf-8' })
 
-console.log(content.replace(/%colors%/gmi, res))
+writeFileSync(join(__dirname, '..', 'readme.tmpl'), content.replace(/%colors%/gmi, res))
+
+console.log('✅ README.md has been updated')
